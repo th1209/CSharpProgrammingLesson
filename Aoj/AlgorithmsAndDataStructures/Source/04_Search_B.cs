b35bc75937d8f7af09393de8ab5571e51746a018
Toshiki Hata <t.hata@oratta.net>
2017-11-26 19:29:04 +0900

    探索

---
+++ Aoj/AlgorithmsAndDataStructures/Source/04_Search_B.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Aoj.ALDS.Chapter4B
+{
+    class Program
+    {
+        /// <summary>
+        /// 二分探索して listからvalueのキーを探す。
+        /// 見つからない場合は-1を返す。
+        /// </summary>
+        public static int BinarySearch<T>(List<T> list, T value) where T : IComparable<T>
+        {
+            int left = 0;
+            int right = list.Count;
+
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                int compare = value.CompareTo(list[mid]);
+                if (compare == 0)
+                    return mid;
+                else if (compare < 0)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+
+            return -1;
+        }
+
+        public static void Solve()
+        {
+            //int n = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+            var list1 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).ToList();
+            int q = int.Parse(Console.ReadLine());
+            var list2 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).ToList();
+
+            int counter = 0;
+            for (int i = 0; i < q; i++)
+            {
+                if (BinarySearch(list1, list2[i]) >= 0)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        // public static void Main()
+        // {
+        //     // Console.SetIn(new System.IO.StreamReader("Aoj/AlgorithmsAndDataStructures/Input/04_Search_B_01.txt"));
+        //     // var sw = new Stopwatch();
+        //     // sw.Start();
+
+        //     Solve();
+
+        //     // Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        // }
+    }
+}
---
+++ Aoj/AlgorithmsAndDataStructures/Source/04_Search_C.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Aoj.ALDS.Chapter4C
+{
+    class Program
+    {
+        public static void Solve()
+        {
+            var dic = new Dictionary<string, int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                switch (inputs[0])
+                {
+                    case "insert":
+                        if (! dic.ContainsKey(inputs[1]))
+                            dic[inputs[1]] = 1;
+                        break;
+                    case "find":
+                        Console.WriteLine(dic.ContainsKey(inputs[1]) ? "yes" : "no");
+                        break;
+                    default:
+                        throw new System.IO.IOException("");
+                }
+            }
+        }
+
+        // public static void Main()
+        // {
+        //     Console.SetIn(new System.IO.StreamReader("Aoj/AlgorithmsAndDataStructures/Input/04_Search_C_01.txt"));
+        //     // var sw = new Stopwatch();
+        //     // sw.Start();
+
+        //     Solve();
+
+        //     // Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        // }
+    }
+}
