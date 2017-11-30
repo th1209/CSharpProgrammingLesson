e212883499becdf84639cb04e710dbf1557c9c6e
Toshiki Hata <t.hata@oratta.net>
2017-12-01 01:08:24 +0900

    プリム法、ダイクストラ法(途中)

---
+++ Aoj/AlgorithmsAndDataStructures/Source/12_Graph_A.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Aoj.ALDS.Chapter12A
+{
+    enum Color
+    {
+        WHITE, GRAY, BLACK
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// プリムのアルゴリズムで最小全域木を求める。
+        /// </summary>
+        /// <param name="m">頂点の隣接行列。</param>
+        /// <param name="n">頂点の個数。</param>
+        /// <returns></returns>
+        public static int Prim(List<List<int>> m, int n)
+        {
+            // 隣接する辺のうち、最も小さい値を格納する。
+            var d = new List<int>();
+            // 対象の頂点の親頂点を格納する(始点の値は-1)。
+            var p = new List<int>();
+            // 探索済み状態。BLACKで探索済み。
+            var color = new List<Color>();
+
+            // 各要素を初期化する。
+            for (int i = 0; i < n; i++)
+            {
+                d.Add(Int32.MaxValue);
+                p.Add(-1);
+                color.Add(Color.WHITE);
+            }
+
+            // 最初は適当な頂点から探索を始めるため、d[0]を0にする。
+            d[0] = 0;
+
+            int u;
+            int minV;
+            while (true)
+            {
+                u = -1;
+                minV = Int32.MaxValue;
+
+                // グレーの頂点のうち、d[i]が最も小さい頂点を求める。
+                for (int i = 0; i < n; i++)
+                {
+                    if (minV > d[i] && color[i] != Color.BLACK) {
+                        u = i;
+                        minV = d[i];
+                    }
+                }
+
+                // 全ての頂点を探索し終わった時点でbreak。
+                if (u == -1) break;
+
+                // 隣接する頂点の最小距離と親頂点を更新しておく。
+                for (int i = 0; i < n; i++)
+                {
+                    if (color[i] != Color.BLACK && m[u][i] != -1) {
+                        if (d[i] > m[u][i]){
+                            d[i] = m[u][i];
+                            p[i] = u;
+                            color[i] = Color.GRAY;
+                        }
+                    }
+                }
+
+                // 今回の頂点は探索済みにしておく。
+                color[u] = Color.BLACK;
+            }
+
+            // 合計を求める。
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (p[i] != -1) sum += m[i][p[i]];
+            }
+            return sum;
+        }
+
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var m  = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                m.Add(new List<int>());
+                string line = Console.ReadLine();
+                line = line.Substring(1, line.Length - 1);
+                m[i] = Array.ConvertAll(line.Split(' '), int.Parse).ToList();
+            }
+
+            int answer = Prim(m, n);
+            Console.WriteLine(answer);
+        }
+
+        // public static void Main()
+        // {
+        //     // Console.SetIn(new System.IO.StreamReader("Aoj/AlgorithmsAndDataStructures/Input/12_Graph_A_01.txt"));
+        //     // var sw = new Stopwatch();
+        //     // sw.Start();
+
+        //     Solve();
+
+        //     // Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        // }
+    }
+}
---
+++ Aoj/AlgorithmsAndDataStructures/Source/12_Graph_B.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Aoj.ALDS.Chapter12B
+{
+    enum Color
+    {
+        WHITE, GRAY, BLACK
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// ダイクストラのアルゴリズムにより、始点sから各頂点への最短経路を求める。
+        /// </summary>
+        /// <param name="m">グラフの隣接行列表現。</param>
+        /// <param name="n">頂点の個数。</param>
+        /// <param name="s">始点。</param>
+        /// <returns></returns>
+        public static int[] Dijkstra(int[][]m, int n, int s)
+        {
+            // 始点sからvまでの最小距離。
+            var d = new int[n];
+
+            // 頂点vの最短経路における親の頂点番号。
+            var p = new int[n];
+
+            // 頂点vの探索状態。
+            var c = new Color[n];
+
+            // 各要素を初期化する。
+            for (int i = 0; i < n; i++)
+            {
+                d[i] = Int32.MaxValue;
+                p[i] = -1;
+                c[i] = Color.WHITE;
+            }
+
+            d[s] = 0;
+
+            while (true)
+            {
+                int current = -1;
+                int minCost = Int32.MaxValue;
+
+                // GRAYの頂点のうち、最もコストが低い頂点を選択。
+                for (int i = 0; i < n; i++)
+                {
+                    if (c[i] != Color.BLACK && d[i] < minCost)
+                    {
+                        current = i;
+                        minCost = d[i];
+                    }
+                }
+
+                // 全頂点がBLACKになった時点で抜ける。
+                if (current == -1) break;
+
+                // 隣接する頂点の最小距離が更新できれば更新し、GRAYに塗り替える。
+                for (int next = 0; next < n; next++)
+                {
+                    if (c[next] != Color.BLACK && m[current][next] != -1)
+                    {
+                        if (d[current] + m[current][next] < d[next])
+                        {
+                            d[next] = d[current] + m[current][next];
+                            p[next] = current;
+                            c[next] = Color.GRAY;
+                        }
+                    }
+                }
+
+                // 現在の頂点を探索済みにする。
+                c[current] = Color.BLACK;
+            }
+
+            return d;
+        }
+
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var m  = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                m[i] = Enumerable.Repeat<int>(-1, n).ToArray();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int u = input[0];
+                for (int j = 2; j < input.Count(); j += 2)
+                {
+                    int v = input[j];
+                    int d = input[j+1];
+                    m[u][v] = d;
+                }
+            }
+
+            int[] answer = Dijkstra(m, n, 0);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0} {1}", i, answer[i]);
+            }
+        }
+
+        // public static void Main()
+        // {
+        //     // Console.SetIn(new System.IO.StreamReader("Aoj/AlgorithmsAndDataStructures/Input/12_Graph_B_01.txt"));
+        //     // var sw = new Stopwatch();
+        //     // sw.Start();
+
+        //     Solve();
+
+        //     // Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        // }
+    }
+}
